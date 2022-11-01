3bace763827192a602e462b567879a7ba6b323c1
hzttw <zh_01040@163.com>
2022-11-01 23:05:34 +0800

    feat : test push

---
+++ demo/src/main.jsx
@@ -0,0 +1,11 @@
+import { ReactDOM } from "./which-react";
+import "./index.css";
+
+const jsx = (
+  <div className="border">
+    <h1>react</h1>
+    <a href="https://github.com/bubucuo/mini-react">mini react</a>
+  </div>
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(jsx);
