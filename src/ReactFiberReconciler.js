de2ec5228fd155b8a8be8e65b363f04b1ae8efa1
hzttw <zh_01040@163.com>
2023-01-30 22:49:10 +0800

    feat: 实现原生节点初次渲染

--- src/ReactFiberReconciler.js
+++ src/ReactFiberReconciler.js
@@ -1,5 +1,40 @@
-export function updateHostComponent() {}
+import { createFiber } from './ReactFiber';
+import { isArray, isStringOrNumber, updateNode } from './utils';
+
+//处理原生标签
+export function updateHostComponent(wip) {
+  if (!wip.stateNode) {
+    wip.stateNode = document.createElement(wip.type);
+    updateNode(wip.stateNode,wip.props)
+  }
+  reconcileChildren(wip, wip.props.children);
+}
 export function updateFunctionComponent() {}
 export function updateClassComponent() {}
 export function updateFragmentComponent() {}
 export function updateHostTextComponent() {}
+
+//协调（核心是diff）
+function reconcileChildren(wip, children) {
+  if (isStringOrNumber(children)) {
+    return;
+  }
+  const newChildren = isArray(children) ? children : [children];
+  let perviousNewFiber = null;
+  for (let i = 0; i < newChildren.length; i++) {
+    const newChild = newChildren[i];
+    if (newChild == null) {
+      continue;
+    }
+    const newFiber = createFiber(newChild, wip);
+
+    //判断头节点，如何判断头节点
+    if (perviousNewFiber === null) {
+      wip.child = newFiber;
+    }else{
+      perviousNewFiber.sibling = newFiber
+    }
+
+    perviousNewFiber = newFiber;
+  }
+}
--- src/ReactFiberWorkLoop.js
+++ src/ReactFiberWorkLoop.js
@@ -12,11 +12,12 @@ import {
   HostComponent,
   HostText,
 } from './ReactWorkTags';
+import { Placement } from './utils';
 
 let wip = null; //work in progress 当前正在工作中的，当前正在执行的fiber
 let wipRoot = null;
 //初次渲染和更新
-export function schedilUpdateOnFiber(fiber) {
+export function scheduleUpdateOnFiber(fiber) {
   wip = fiber;
   wipRoot = fiber;
 }
@@ -57,4 +58,41 @@ function performUnitOfWork() {
     next = next.return;
   }
   wip = null;
+}
+
+function wookLoop(IdleDeadline) {
+  while (wip && IdleDeadline.timeRemaining() > 0) {
+    performUnitOfWork();
+  }
+
+  if (!wip && wipRoot) {
+    commitRoot();
+  }
+}
+requestIdleCallback(wookLoop);
+//提交
+function commitRoot() {
+console.log('wipRoot',wipRoot);
+
+  commitWorker(wipRoot);
+  wipRoot = null;
+}
+function commitWorker(wip) {
+  //如果没有节点就退出
+  if (!wip) {
+    return;
+  }
+  //1.提交自己
+  // parentNode是父dom节点
+  const parentNode = wip.return.stateNode;
+  const { flags, stateNode } = wip;
+  console.log('wip',wip);
+  if (flags & Placement && stateNode) {
+    console.log(stateNode);
+    parentNode.appendChild(stateNode);
+  }
+  //2.提交子节点
+  commitWorker(wip.child);
+  //3.提交兄弟
+  commitWorker(wip.sibling);
 }
